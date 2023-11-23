dea79aa4cd275ac79b2c648d38819e3878122d42
MJAntenor <mjoantenor@gmail.com>
2023-11-22 22:44:58 -0600

    Set up Horizontal Navbar

--- happy_bites/src/Navbar.js
+++ happy_bites/src/Navbar.js
@@ -1,6 +1,15 @@
 const Navbar = () => {
     return ( 
-        
+        <nav className="navbar">
+            <h1>HappyBites</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/profile">Profile</a>
+                <a href="/cookbook">Cookbook</a>
+                <a href="/create">New Recipe</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
      );
 }
  
